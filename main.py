3f5681bb4f5adba4af07557c2a468989a8649135
verykumar <varunkumaranalysis@gmail.com>
2024-01-12 17:26:41 -0500

    fixed api location

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import streamlit as st
 from memory_module import cast_memory, story_memory
 from chains_module import cast_chain, arc_chain, quest_chain, act_chain, story_chain
 
-os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
+os.environ['OPENAI_API_KEY'] = st.secrets
 
 # App Structure
 st.title('Visual_Novel_StoryGen')
